c8ec8f40a735cdce48d93cafb4d2498f68ab8ea0
gizemsangur1 <gizem2002sangur@gmail.com>
2025-08-09 13:55:25 +0300

    Create Instagram posts

--- src/components/Checkout/page.tsx
+++ src/components/Checkout/page.tsx
@@ -1,27 +1,77 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
+import InstagramPost from "../InstagramPost/page";
+import Post1 from "../../../public/post.png";
+import { Instagram } from "@mui/icons-material";
 
 export default function CheckoutPage() {
   return (
     <Grid
       container
       sx={{
-        marginTop: "60px",
+        margin: "60px 0",
         display: "flex",
         textAlign: "center",
         width: "100%",
         justifyContent: "center",
       }}
     >
-      <Grid sx={{ margin: "20px 0" }}>
+      <Grid sx={{ margin: "30px 0" }}>
         <Typography sx={{ fontSize: "48px", fontWeight: 500 }}>
           Check out @foodieland on Instagram
         </Typography>
-        <Typography sx={{ fontSize: "16px" ,opacity:0.6,marginTop:"10px"}}>
+        <Typography sx={{ fontSize: "16px", opacity: 0.6, marginTop: "10px" }}>
           Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
           tempor incididunt ut labore et dolore <br /> magna aliqut enim ad
           minim{" "}
         </Typography>
+      </Grid>
+      <Grid
+        size={12}
+        sx={{
+          padding: "10px 50px",
+          background:
+            "linear-gradient(to bottom, rgba(255,255,255,1), #E7F9FD)",
+			marginTop:"40px"
+        }}
+      >
+        <Grid size={12}
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          padding: "10px 50px",
+        }}>
+          <InstagramPost
+            text="The vegetables dishes need to have certain vitamin for those people"
+            image={Post1}
+          />
+          <InstagramPost
+            text="Sweet food can bring someon into happiness as long as they don’t eat to much"
+            image={Post1}
+          />
+          <InstagramPost
+            text="What are you doing before start cooking? prepare the  tools or ingredients?"
+            image={Post1}
+          />
+          <InstagramPost
+            text="Steak never be wrong, it’s suitable for you who want romantic dinner"
+            image={Post1}
+          />
+        </Grid>
+
+        <Grid size={12}>
+          <Button
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+              borderRadius: "20px",
+              padding: "18px 20px",
+			  margin:"50px 0"
+            }}
+          >
+            Visit Our Instagram <Instagram />
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/components/InstagramPost/page.tsx
+++ src/components/InstagramPost/page.tsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import { MoreHoriz } from "@mui/icons-material";
+import { Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
 
-export default function InstagramPost() {
+import like from "../../../public/like.svg";
+import comment from "../../../public/comment.svg";
+import send from "../../../public/send.svg";
+import save from "../../../public/save.svg";
+import pagination from "../../../public/pagination.svg";
+
+type Props = {
+  text: string;
+  image: any;
+};
+
+export default function InstagramPost({
+  text,
+  image,
+}: Props) {
   return (
-	<div>InstagramPost</div>
-  )
+    <Grid container sx={{ borderRadius: "5px", padding: "10px" }}>
+      <Grid
+        size={12}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Grid sx={{ display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              width: "35px",
+              height: "35px",
+              backgroundColor: "blue",
+              borderRadius: "50%",
+              margin: "10px",
+            }}
+          ></div>
+          <Grid sx={{ marginLeft: "10px", textAlign: "start" }}>
+            <Typography sx={{ fontSize: "11px", fontWeight: 600 }}>
+              Foodieland.
+            </Typography>
+            <Typography sx={{ fontSize: "8px" }}>Tokyo, Japan</Typography>
+          </Grid>
+        </Grid>
+
+        <MoreHoriz />
+      </Grid>
+      <Grid size={12}>
+        <Image
+          alt="post"
+          src={image}
+          style={{
+            width: "100%",
+            height: "100%", 
+            objectFit: "cover",
+          }}
+        />
+      </Grid>
+      <Grid
+        size={12}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        <Grid>
+          <Image src={like} alt="icon" style={{ marginRight: "10px" }} />
+          <Image src={comment} alt="icon" style={{ marginRight: "10px" }} />
+          <Image src={send} alt="icon" style={{ marginRight: "10px" }} />
+        </Grid>
+        <Grid>
+          <Image src={pagination} alt="icon" />
+        </Grid>
+        <Grid>
+          <Image src={save} alt="icon" />
+        </Grid>
+      </Grid>
+      <Grid size={12} sx={{display:"flex",alignItems:"center",padding:"0 10px"}}>
+		<div style={{backgroundColor:"black",width:"20px",height:"20px",borderRadius:"50%"}}></div>
+		<Typography sx={{fontSize:"10px",marginLeft:"10px"}}>Liked by <b>craig_love </b> and <b>44,686</b></Typography>
+	  </Grid>
+      <Grid size={12} sx={{textAlign:"start",paddingLeft:"10px",marginTop:"10px"}}>
+		<Typography sx={{fontSize:"10px"}}><b>Foodieland.</b> {text}</Typography>
+	  </Grid>
+    </Grid>
+  );
 }
